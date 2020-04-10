98ba2df1208ce3255afe30414d1e9da798779dac
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-04-10 15:45:26 -0400

    start to bring tsibble in

--- analysis/misc.R
+++ analysis/misc.R
@@ -13,3 +13,41 @@ hydro_observations %>%
 hydro_observations %>%
   group_by(wday) %>%
   summarize(usage = sum(consumption_k_wh), cost = sum(cost))
+
+hydro_observations %>%
+  mutate(month = floor_date(date, "months")) %>%
+  group_by(month) %>%
+  summarize(usage = sum(consumption_k_wh)) %>%
+  filter(month < "2020-04-01") %>%
+  ggplot(aes(x = month, y = usage)) +
+  geom_line() +
+  geom_smooth() +
+  scale_x_date(date_breaks = "6 months", date_labels = "%b '%y")
+
+hydro_observations %>%
+  filter(hour(time) > 8) %>%
+  filter(hour(time) <= 18) %>%
+  mutate(month = floor_date(date, "months")) %>%
+  group_by(month) %>%
+  summarize(usage = sum(consumption_k_wh)) %>%
+  filter(month < "2020-04-01") %>%
+  ggplot(aes(x = month, y = usage)) +
+  geom_line() +
+  geom_smooth() +
+  scale_x_date(date_breaks = "6 months", date_labels = "%b '%y")
+
+hydro_observations %>%
+  filter(rate_type == "On-Peak") %>%
+  mutate(month = floor_date(date, "months")) %>%
+  group_by(month) %>%
+  summarize(usage = sum(consumption_k_wh)) %>%
+  filter(month < "2020-04-01") %>%
+  ggplot(aes(x = month, y = usage)) +
+  geom_line() +
+  geom_smooth() +
+  scale_x_date(date_breaks = "6 months", date_labels = "%b '%y")
+
+hydro_observations %>%
+  mutate(usage_bin = cut(consumption_k_wh, breaks = 5)) %>%
+  mutate(hr = hour(time)) %>%
+  count_group(usage_bin, hr)
--- load.R
+++ load.R
@@ -1,13 +1,18 @@
 library(tidyverse)
 library(readxl)
 library(janitor)
+
 library(lubridate)
+library(tsibble)
+library(feasts)
+library(fable)
+
 library(helpers)
 
 hydro_observations <- fs::dir_ls("data/source/hydroottawa.com/", regexp = "*.xls") %>%
   map_dfr(read_excel, skip = 2, .id = "source_file") %>%
   clean_names() %>%
   arrange(time) %>%
-  mutate(date = as_date(date)) %>%
-  distinct(date, time, rate_type, consumption_k_wh, cost) %>%
-  mutate(year = year(date), month = month(date), wday = wday(date, week_start = 1, label = TRUE))
+  distinct(time, rate_type, consumption_k_wh, cost) %>%
+  as_tsibble(index = time)
+  mutate(year = year(date), month = month(date, label = TRUE), wday = wday(date, week_start = 1, label = TRUE))
