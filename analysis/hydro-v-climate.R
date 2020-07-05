66a42607538e25befae959f110d2cdc21fa9c6a8
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-07-05 19:05:46 -0400

    join the data!

--- analysis/hydro-v-climate.R
+++ analysis/hydro-v-climate.R
@@ -1,4 +1,8 @@
 source("load.R")
 
-hydro_observations %>%
-  left_join(climate_observations %>% select(date_time, ))
+hydro_climate_observations <- hydro_observations %>%
+  left_join(
+    climate_observations %>%
+      select(date_time, temp_c:wind_chill),
+    by = c("time" = "date_time")
+  )
